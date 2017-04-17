f0c6ceb43d4b03f108f13e09f49bfe5c7335eb82
Roshni Biswas <roshnibiswas079@gmail.com>
2017-04-17 11:55:12 +0530

    Scraping weather data using Beautiful Soup

---
+++ Weather_scrape.py
@@ -0,0 +1,53 @@
+#Weather in Pasadena
+#inspired by https://www.dataquest.io/blog/web-scraping-tutorial-python/
+
+import requests
+
+page = requests.get("http://forecast.weather.gov/MapClick.php?lon=-118.15334312304958&lat=34.14724265183901")
+soup = BeautifulSoup (page.content, 'html.parser')
+seven_day = soup.find(id="seven-day-forecast")
+forecast_items = seven_day.find_all(class_="tombstone-container")
+tonight = forecast_items[0]
+#print(tonight.prettify())
+
+period = tonight.find(class_="period-name").get_text()
+short_desc = tonight.find(class_="short-desc").get_text()
+temp = tonight.find(class_="temp").get_text()
+
+print(period)
+print(short_desc)
+print(temp)
+
+img = tonight.find("img")
+desc = img['title']
+
+print(desc)
+
+period_tags = seven_day.select(".tombstone-container .period-name")
+periods = [pt.get_text() for pt in period_tags]
+periods
+
+short_descs = [sd.get_text() for sd in seven_day.select(".tombstone-container .short-desc")]
+temps = [t.get_text() for t in seven_day.select(".tombstone-container .temp")]
+descs = [d["title"] for d in seven_day.select(".tombstone-container img")]
+
+print(short_descs)
+print(temps)
+print(descs)
+
+
+import pandas as pd
+weather = pd.DataFrame({
+        "period": periods, 
+        "short_desc": short_descs, 
+        "temp": temps, 
+        "desc":descs
+    })
+
+weather
+
+temp_nums = weather["temp"].str.extract("(?P<temp_num>\d+)", expand=False)
+weather["temp_num"] = temp_nums.astype('int')
+temp_nums
+
+weather["temp_num"].mean()
